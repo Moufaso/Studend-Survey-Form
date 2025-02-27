f6854c75a11b6d4ec452af6dcc4b6c0eef321257
Moufaso <Saman3AH@gmail.com>
2025-02-27 13:49:54 -0500

    Readme added

---
+++ Readme.txt
@@ -0,0 +1,2 @@
+HW2 Documentation
+Saman Hosseini
--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function splitRaffle() {
    var tokens = document.getElementById("raffle").value.split(",");
    for (let token of tokens) {
       if (parseInt(token) == 3) {
-         window.alert("You won!");
+         window.alert("You won a ticket!");
          return;
       }
    }
